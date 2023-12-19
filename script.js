6bab44713cf0e299014d4e8c04f4302776cd98dd
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-19 18:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstWord(s) {
-  // your code here
+ function firstWord(s) {
+    let str = '';
+    for (let i = 0; i < s.length; i++) {
+        const item = s[i];
+        str += item;
+        if (item === ' ') {
+            break;
+        }
+    }
+    return str.trim(); // Trim to remove leading/trailing spaces
 }
 
-// Do not change the code below
-
+// Do not change the code below.
 const s = prompt("Enter String:");
 alert(firstWord(s));
